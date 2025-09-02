2431ac362178c0266ca4d1dc2bf7805570c015b3
井上 翔太 <631101@np.group>
2025-09-02 11:38:21 +0900

    storage追加

--- src/old_main.tsx
+++ src/old_main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Authenticator } from '@aws-amplify/ui-react';
 import App from "./App.tsx";
+import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css';
