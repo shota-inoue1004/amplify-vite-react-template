aab6ff3f3b704ad764a0c269abe8e90ae1ed5518
井上 翔太 <631101@np.group>
2025-09-02 17:24:49 +0900

    update

--- src/components/AuthWithSAML.tsx
+++ src/components/AuthWithSAML.tsx
@@ -3,28 +3,21 @@ import { Button, Text, Loader, useAuthenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 import { signInWithRedirect } from 'aws-amplify/auth';
-import { useTranslation } from 'react-i18next';
-
-const samlCognitoDomainName: string = import.meta.env
-  .VITE_APP_SAML_COGNITO_DOMAIN_NAME;
-const samlCognitoFederatedIdentityProviderName: string = import.meta.env
-  .VITE_APP_SAML_COGNITO_FEDERATED_IDENTITY_PROVIDER_NAME;
-const speechToSpeechEventApiEndpoint: string = import.meta.env
-  .VITE_APP_SPEECH_TO_SPEECH_EVENT_API_ENDPOINT;
-
+import outputs from "../../amplify_outputs.json";
+const cognitoUserPoolId: string = outputs.auth.user_pool_id;
+const cognitoUserPoolCientId: string = outputs.auth.user_pool_client_id;
+const cognitoIdentityPoolId: string = outputs.auth.identity_pool_id;
+const samlCognitoDomainName: string = outputs.auth.oauth.domain;
+const samlCognitoFederatedIdentityProviderName: string = "MicrosoftEntraIDSAML";
 type Props = {
   children: React.ReactNode;
 };
-
 const AuthWithSAML: React.FC<Props> = (props) => {
-  const { t } = useTranslation();
   const { authStatus } = useAuthenticator((context) => [context.authStatus]);
-
   const [authenticated, setAuthenticated] = useState(false);
   const [loading, setLoading] = useState(true);
-
   useEffect(() => {
-    // Verify the authentication status
+    // 認証状態の検証
     if (authStatus === 'configuring') {
       setLoading(true);
       setAuthenticated(false);
@@ -36,7 +29,6 @@ const AuthWithSAML: React.FC<Props> = (props) => {
       setAuthenticated(false);
     }
   }, [authStatus]);
-
   const signIn = () => {
     signInWithRedirect({
       provider: {
@@ -44,18 +36,17 @@ const AuthWithSAML: React.FC<Props> = (props) => {
       },
     });
   };
-
   Amplify.configure({
     Auth: {
       Cognito: {
-        userPoolId: import.meta.env.VITE_APP_USER_POOL_ID,
-        userPoolClientId: import.meta.env.VITE_APP_USER_POOL_CLIENT_ID,
-        identityPoolId: import.meta.env.VITE_APP_IDENTITY_POOL_ID,
+        userPoolId: cognitoUserPoolId,
+        userPoolClientId: cognitoUserPoolCientId,
+        identityPoolId: cognitoIdentityPoolId,
         loginWith: {
           oauth: {
-            domain: samlCognitoDomainName, // Specify the value in cdk.json
+            domain: samlCognitoDomainName, // cdk.json の値を指定
             scopes: ['openid', 'email', 'profile'],
-            // Get the Web page deployed with CloudFront dynamically
+            // CloudFront で展開している Web ページを動的に取得
             redirectSignIn: [window.location.origin],
             redirectSignOut: [window.location.origin],
             responseType: 'code',
@@ -63,30 +54,24 @@ const AuthWithSAML: React.FC<Props> = (props) => {
         },
       },
     },
-    API: {
-      Events: {
-        endpoint: speechToSpeechEventApiEndpoint,
-        region: process.env.VITE_APP_REGION!,
-        defaultAuthMode: 'userPool',
-      },
-    },
   });
-
   return (
     <>
       {loading ? (
         <div className="grid grid-cols-1 justify-items-center gap-4">
-          <Text className="mt-12 text-center">{t('auth.loading')}</Text>
+          <Text className="mt-12 text-center">Loading...</Text>
           <Loader width="5rem" height="5rem" />
         </div>
       ) : !authenticated ? (
         <div className="grid grid-cols-1 justify-items-center gap-4">
-          <Text className="mt-12 text-center text-3xl">{t('auth.title')}</Text>
+          <Text className="mt-12 text-center text-3xl">
+            Todo App
+          </Text>
           <Button
             variation="primary"
             onClick={() => signIn()}
             className="mt-6 w-60">
-            {t('auth.login')}
+            ログイン
           </Button>
         </div>
       ) : (
@@ -95,5 +80,4 @@ const AuthWithSAML: React.FC<Props> = (props) => {
     </>
   );
 };
-
 export default AuthWithSAML;
