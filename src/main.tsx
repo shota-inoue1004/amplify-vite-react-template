7744d27c98c866aa0dd7636bf46a0ee30cb84d3d
井上 翔太 <631101@np.group>
2025-09-02 17:01:37 +0900

    update Authenticator

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,19 @@ import App from "./App.tsx";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css';
-import { signInWithRedirect } from "aws-amplify/auth" // 追記
+//import { signInWithRedirect } from "aws-amplify/auth" 
+import AuthWithSAML from "./components/AuthWithSAML.tsx"; // 追記
+import { Authenticator } from '@aws-amplify/ui-react'; // 追記
 Amplify.configure(outputs);
-signInWithRedirect({ // 追記
-  provider: { custom: "MicrosoftEntraIDSAML" } // 追記
-}) // 追記
+//signInWithRedirect({ // 追記
+//provider: { custom: "MicrosoftEntraIDSAML" } // 追記
+//}) // 追記
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
+    <Authenticator.Provider> 
+      <AuthWithSAML> 
+        <App />
+      </AuthWithSAML> 
+    </Authenticator.Provider>
   </React.StrictMode>
 );
