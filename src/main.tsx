e8a6ded3263a35ad9279b88895d2578f4909ba1c
井上 翔太 <631101@np.group>
2025-09-02 16:06:28 +0900

    EntraID認証設定

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import App from "./App.tsx";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css';
-
+import { signInWithRedirect } from "aws-amplify/auth" // 追記
 Amplify.configure(outputs);
-
+signInWithRedirect({ // 追記
+  provider: { custom: "MicrosoftEntraIDSAML" } // 追記
+}) // 追記
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Authenticator>
