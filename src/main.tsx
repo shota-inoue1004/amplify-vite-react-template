1d864c5c1331cfccd79080a926f0267cdc328ebe
井上 翔太 <631101@np.group>
2025-09-04 13:14:41 +0900

    main更新

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+//import App from "./App.tsx";
+import {
+  createAmplifyAuthAdapter,
+  createStorageBrowser,
+} from '@aws-amplify/ui-react-storage/browser';
+import "@aws-amplify/ui-react-storage/styles.css";
 import "./index.css";
 import { Amplify } from "aws-amplify";
-import config from "../amplify_outputs.json";
+import outputs from "../amplify_outputs.json";
 // import { signInWithRedirect } from "aws-amplify/auth" // コメントアウト
-import AuthWithSAML from "./components/AuthWithSAML.tsx"; // 追記
-import { Authenticator } from '@aws-amplify/ui-react'; // 追記
-
-
-Amplify.configure({
-  ...(config as any),
-  Storage: {
-    AWSS3: {
-      bucket: config.storage.bucket_name,
-      region: config.storage.aws_region
-    },
-    defaultAccessLevel: "protected"
-  }
-});
-
-
+//import AuthWithSAML from "./components/AuthWithSAML.tsx"; // 追記
+//import { Authenticator } from '@aws-amplify/ui-react'; // 追記
+Amplify.configure(outputs);
 // signInWithRedirect({ // コメントアウト
 //   provider: { custom: "MicrosoftEntraIDSAML" }  // コメントアウト
 // })  // コメントアウト
+
+
+export const { StorageBrowser } = createStorageBrowser({
+  config: createAmplifyAuthAdapter(),
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator.Provider>
-      <AuthWithSAML> 
-        <App />
-      </AuthWithSAML> 
-    </Authenticator.Provider> 
+        <StorageBrowser/>
   </React.StrictMode>
 );
