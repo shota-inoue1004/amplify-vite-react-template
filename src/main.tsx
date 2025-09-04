630c1a0a05d3e6fde5e12f6d6be8a9e144b5bf4e
井上 翔太 <631101@np.group>
2025-09-04 11:18:00 +0900

    App更新

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import config from "../amplify_outputs.json";
 // import { signInWithRedirect } from "aws-amplify/auth" // コメントアウト
 import AuthWithSAML from "./components/AuthWithSAML.tsx"; // 追記
 import { Authenticator } from '@aws-amplify/ui-react'; // 追記
-Amplify.configure(outputs);
+Amplify.configure(config);
 // signInWithRedirect({ // コメントアウト
 //   provider: { custom: "MicrosoftEntraIDSAML" }  // コメントアウト
 // })  // コメントアウト
