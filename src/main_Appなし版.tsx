5436ba821b8a74f4e9f0a19c199ac175250377fa
井上 翔太 <631101@np.group>
2025-09-04 14:39:58 +0900

    Appなしテスト

--- src/main_Appなし版.tsx
+++ src/main_Appなし版.tsx
@@ -1,29 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-//import App from "./App.tsx";
-import "./index.css";
+"use client";
+
 import { Amplify } from "aws-amplify";
 import { 
   createAmplifyAuthAdapter,
   createStorageBrowser,
 } from "@aws-amplify/ui-react-storage/browser";
+import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react-storage/styles.css";
 import outputs from "../amplify_outputs.json";
-// import { signInWithRedirect } from "aws-amplify/auth" // コメントアウト
-import AuthWithSAML from "./components/AuthWithSAML.tsx"; // 追記
-import { Authenticator } from '@aws-amplify/ui-react'; // 追記
+import AuthWithSAML from "./components/AuthWithSAML.tsx";
+
 Amplify.configure(outputs);
 
 export const { StorageBrowser } = createStorageBrowser({
   config: createAmplifyAuthAdapter(),
 });
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+export default function App() {
+  return (
     <Authenticator.Provider>
       <AuthWithSAML> 
-      <StorageBrowser/>
+        <App />
       </AuthWithSAML> 
     </Authenticator.Provider> 
-  </React.StrictMode>
-);
+  );
+}
