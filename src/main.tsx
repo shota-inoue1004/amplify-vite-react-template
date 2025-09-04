c1e743aa2ea4da6f0530b35b898f1dfffa62d784
井上 翔太 <631101@np.group>
2025-09-04 15:34:37 +0900

    Appなしテスト

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import { 
@@ -9,19 +8,22 @@ import {
 } from "@aws-amplify/ui-react-storage/browser";
 import "@aws-amplify/ui-react-storage/styles.css";
 import outputs from "../amplify_outputs.json";
-// import { signInWithRedirect } from "aws-amplify/auth" // コメントアウト
 import AuthWithSAML from "./components/AuthWithSAML.tsx"; // 追記
 import { Authenticator } from '@aws-amplify/ui-react'; // 追記
+import { useAuthenticator } from '@aws-amplify/ui-react';
 Amplify.configure(outputs);
 
 export const { StorageBrowser } = createStorageBrowser({
   config: createAmplifyAuthAdapter(),
 });
 
+  const { signOut } = useAuthenticator();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Authenticator.Provider>
       <AuthWithSAML> 
+      <button onClick={signOut}>Sign out</button>
       <h1>てすと</h1>
       </AuthWithSAML> 
     </Authenticator.Provider> 
