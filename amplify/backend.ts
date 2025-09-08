0b7ea962bbdf61862535ab14007252dde0c6620a
井上 翔太 <631101@np.group>
2025-09-08 14:48:38 +0900

    resource更新

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { storage } from './storage/resource';
+import { auth } from './auth/resource.ts';
+import { storage } from './storage/resource.ts';
 
 defineBackend({
   auth,
