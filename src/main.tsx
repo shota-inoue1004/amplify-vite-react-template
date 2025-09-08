a245403bd9133c936fd48c9dfe8f4efdbd53a772
井上 翔太 <631101@np.group>
2025-09-08 10:13:06 +0900

    appなし

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import App from "./App.tsx";
+import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
-import { 
-  createAmplifyAuthAdapter,
-  createStorageBrowser,
-} from "@aws-amplify/ui-react-storage/browser";
-import "@aws-amplify/ui-react-storage/styles.css";
 import outputs from "../amplify_outputs.json";
 // import { signInWithRedirect } from "aws-amplify/auth" // コメントアウト
-//import AuthWithSAML from "./components/AuthWithSAML.tsx"; // 追記
-//import { Authenticator } from '@aws-amplify/ui-react'; // 追記
+import AuthWithSAML from "./components/AuthWithSAML.tsx"; // 追記
+import { Authenticator } from '@aws-amplify/ui-react'; // 追記
 Amplify.configure(outputs);
-
-export const { StorageBrowser } = createStorageBrowser({
-  config: createAmplifyAuthAdapter(),
-});
-
+// signInWithRedirect({ // コメントアウト
+//   provider: { custom: "MicrosoftEntraIDSAML" }  // コメントアウト
+// })  // コメントアウト
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-      <StorageBrowser/>
+    <Authenticator.Provider>
+      <AuthWithSAML> 
+        <App />
+      </AuthWithSAML> 
+    </Authenticator.Provider> 
   </React.StrictMode>
 );
